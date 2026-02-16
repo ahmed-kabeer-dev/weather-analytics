b8987d1f170e684e6875cb3829026be379aa1169
ahmedkabeer <ahmedkabeer.x@gmail.com>
2026-02-16 13:16:56 +0530

    chore: updated CORS with production Vercel URL

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://weather-analytics.vercel.app'
+  ],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Authorization', 'Content-Type']
 }));
